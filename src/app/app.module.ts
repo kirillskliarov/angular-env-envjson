66f87950abfe434fe4597c80c07f21603f03afb4
user <user@example.com>
2021-09-18 00:25:40 +0300

    refactor providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule} from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import {environmentProvider} from "./core/providers/environment-provider";
+import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import {ENVIRONMENT_TOKEN} from "./core/tokens/environment-token";
+import {environmentFactory} from "./core/factories/environment-factory";
 
 @NgModule({
   declarations: [
@@ -14,8 +15,13 @@ import {HttpClientModule} from "@angular/common/http";
     HttpClientModule,
   ],
   providers: [
-    environmentProvider,
+    {
+      provide: ENVIRONMENT_TOKEN,
+      useFactory: environmentFactory,
+      deps: [],
+    },
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/core/providers/environment-provider.ts
+++
@@ -1,9 +0,0 @@
-import {FactoryProvider} from "@angular/core";
-import {ENVIRONMENT_TOKEN} from "../tokens/environment-token";
-import {environmentFactory} from "../factories/environment-factory";
-
-export const environmentProvider: FactoryProvider = {
-  provide: ENVIRONMENT_TOKEN,
-  useFactory: environmentFactory,
-  deps: [],
-}
